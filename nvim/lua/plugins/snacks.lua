cbbe93b1cd96ecee1547ff357fd334a25dc96725
Matheus Mota <matheus.mota@eumedicoresidente.com.br>
2025-07-03 23:23:18 -0300

    refactor(snacks): remove unnecessary bigfile configuration

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,7 +7,6 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
     dashboard = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
@@ -15,7 +14,6 @@ return {
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
     terminal = { enabled = true },
